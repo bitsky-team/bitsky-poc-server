bf983cc2a5206671f3803d86328cc5de86a8b62e
Bitsky Team <teambitsky@gmail.com>
2019-02-01 15:03:02 +0100

    Comments & Hardware tests

--- tests/app/controller/HardwareControllerTest.php
+++ tests/app/controller/HardwareControllerTest.php
@@ -19,11 +19,28 @@
             $_POST['token'] = $admin['token'];
             $_POST['uniq_id'] = $admin['uniq_id'];
 
-            // Get posts
+            // Get temp
             $result = $hardwareController->getTemp();
             $result = json_decode($result, true);
 
             $this->assertTrue($result['success']);
             $this->assertTrue(is_int($result['temperature']));
+        }
+
+        public function testGetCentralProcessingUnitUsage() : void
+        {
+            Kernel::bootEloquent();
+            $hardwareController = new HardwareController();
+
+            // Get Admin Account
+            $admin = UserModel::where('rank', 2)->first();
+            $_POST['token'] = $admin['token'];
+            $_POST['uniq_id'] = $admin['uniq_id'];
+
+            $result = $hardwareController->getCPUUsage();
+            $result = json_decode($result, true);
+
+            $this->assertTrue($result['success']);
+            $this->assertTrue(is_double($result['cpu_usage']));
         }
     }

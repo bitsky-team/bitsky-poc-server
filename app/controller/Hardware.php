39a3783f3c1ed436ababc977a4988fc3643c93a3
root <root@bitsky>
2019-05-05 16:10:00 +0200

    Devices can now have no number

--- app/controller/Hardware.php
+++ app/controller/Hardware.php
@@ -105,13 +105,39 @@ class Hardware extends Controller
             $path = '/dev/sd';
             $numberOfPorts = 4;
 
-            for($i = 1; $i <= $numberOfPorts; $i++)
+            foreach ($letters as $letter)
             {
-                foreach ($letters as $letter)
+                $noMatch = true;
+
+                for($i = 1; $i <= $numberOfPorts; $i++)
                 {
                     if(file_exists($path . $letter . $i))
                     {
+                        $noMatch = false;
+
                         $name = $path . $letter . $i;
+
+                        $totalMem = str_replace(PHP_EOL, null, shell_exec("echo '{password}' | sudo -S df -h " . $name . "| awk 'NR > 1 {print $2}'"));
+
+                        $freeMem = str_replace(PHP_EOL, null, shell_exec("echo '{password}' | sudo -S df -h " . $name . "| awk 'NR > 1 {print $3}'"));
+
+                        $percent = str_replace(PHP_EOL, null, shell_exec("echo '{password}' | sudo -S df " . $name . "| awk 'NR > 1 {print $5}'"));
+
+                        if(!is_null($totalMem) && !is_null($freeMem) && !is_null($percent))
+                        {
+                            array_push($devices, ['name' => $name, 'totalMem' => $totalMem, 'usedMem' => $freeMem, 'percent' => $percent]);
+                        }else
+                        {
+                            $this->forbidden('nullResult');
+                        }
+                    }
+                }
+
+                if($noMatch)
+                {
+                    if(file_exists($path . $letter))
+                    {
+                        $name = $path . $letter;
 
                         $totalMem = str_replace(PHP_EOL, null, shell_exec("echo '{password}' | sudo -S df -h " . $name . "| awk 'NR > 1 {print $2}'"));
 
